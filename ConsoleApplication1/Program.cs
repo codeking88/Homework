891c87aa12d7b0c3d99ef9b1a559e9ac777314e8
MarketKing <MarketKing@DESKTOP-UHIAI28>
2016-10-10 19:17:15 -0400

    Add project files.

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("The Area of A Rectangle");
+            Console.WriteLine("What is the Length of the Rectangle?");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The length is", length);
+            Console.WriteLine("What is the Width of the Rectangle?");
+            int width = Convert.ToInt32(Console.ReadLine());
+            int AreaOfRectangle = length * width;
+            Console.WriteLine("The Area of the Rectangle is {0}", AreaOfRectangle);
+        }
+    }
+}
